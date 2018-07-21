9cd2b585ae61bfcc4f67a29bf7a0f060e4c39930
simon xie <simon.xie@codewalker.me>
2018-07-21 14:02:47 +0800

    Site updated: 2018-07-21 14:02:35

---
+++ downloads/code/first_module.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date    : 2018-07-21 12:59:29
+# @Author  : Simon (simon.xie@codewalker.meg)
+# @Link    : http://www.codewalker.me
+# @Version : 1.0.0
+
+if __name__ == "__main__":
+    print(f'Run first as {__name__}')
+else:
+    print(f'Run first as Import {__name__}')
---
+++ downloads/code/second_module.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date    : 2018-07-21 13:00:31
+# @Author  : Simon (simon.xie@codewalker.meg)
+# @Link    : http://www.codewalker.me
+# @Version : 1.0.0
+
+import first_module
+
+def second_main():
+    print(f'Run second as {__name__}')
+if __name__ == "__main__":
+    second_main()
